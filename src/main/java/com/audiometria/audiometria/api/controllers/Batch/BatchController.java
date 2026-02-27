b5962c308f394402ad5e2cf3fab33354c91a7d7f
Erick Zambrano (conejo7) <erick_s5490@hotmail.com>
2026-02-26 21:29:53 -0500

    Update BatchController.java

--- src/main/java/com/audiometria/audiometria/api/controllers/Batch/BatchController.java
+++ src/main/java/com/audiometria/audiometria/api/controllers/Batch/BatchController.java
@@ -114,5 +114,16 @@ public class BatchController {
         );
     }
 
+        @GetMapping("/debug/list-volume")
+public ResponseEntity<?> listVolume() {
+
+    File dir = new File("/mnt/uploads");
+
+    if (!dir.exists()) {
+        return ResponseEntity.ok("No existe /mnt/uploads");
+    }
+
+    return ResponseEntity.ok(dir.list());
+}
         
 }
